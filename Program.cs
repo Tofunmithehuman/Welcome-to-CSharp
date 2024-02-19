c3c540f496c6511264397374a06f28f5ca6e2b0d
Bolaji Oluwatofunmi Ashraf <tofunmibolaji13@gmail.com>
2024-02-19 20:52:30 +0100

    CSharp Methods

--- Program.cs
+++ Program.cs
@@ -4,12 +4,34 @@ namespace WelcomeToCSharp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        // Checks if user is admin by checking if name is the expected value
+        public static void checkIsAdmin()
         {
-            Console.WriteLine("Welcome to C#");
             Console.WriteLine("What is your name?");
             string? name = Console.ReadLine();
             Console.WriteLine("Hello " + name);
+
+            if ((name == "Oluwatofunmi") || (name == "oluwatofunmi"))
+            {
+                Console.WriteLine(name + " is an Admin");
+            }
+            else
+            {
+                Console.WriteLine(name + " is not an Admin");
+            }
+
+        }
+        // Add two integer values
+        public static void add(int a, int b)
+        {
+            double sum = a + b;
+            Console.WriteLine(sum);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+            checkIsAdmin();
+            add(10, 3);
         }
     }
 }
